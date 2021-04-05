bfd3cb9f7c766b868831a48cdf3cd3b92e888ef1
igreen1 <iangreen2.00@gmail.com>
2021-04-04 20:54:58 -0700

    Fixed object factories in Our3dObject to allow for mutations to object to render

--- src/Our3DObject.js
+++ src/Our3DObject.js
@@ -5,36 +5,37 @@ const OurMesh = ({ vertices, facesByIndex }, wireframe = false) => {
 
   let isWireframe = wireframe
 
-  const Mesh = {
+  return {
     facesByIndex,
     indexes: vertices,
-    vertices: (isWireframe ? toRawLineArray({vertices, facesByIndex}) : toRawTriangleArray({vertices, facesByIndex})),
-    isWireframe,
-    setWireframe: (wireframe) => isWireframe = wireframe
+    // lines:  toRawLineArray({vertices, facesByIndex}),
+    // triangles: toRawTriangleArray({vertices, facesByIndex}),
+    // get vertices() {return isWireframe ? this.lines : this.triangles}, //pre-calculating reduces run-time but requires more memory. below is less memory solution
+    get vertices() { return (isWireframe ? toRawLineArray({ vertices, facesByIndex }) : toRawTriangleArray({ vertices, facesByIndex })) },
+    get isWireframe() { return isWireframe },
+    setWireframe: (newIsWireframe) => isWireframe = newIsWireframe
   }
-  return Mesh
 }
 
 const Our3DObject = (mesh, colorArrayByVertex) => {
 
-  class ThreeDObject {
-    constructor(mesh, colorArrayByVertex) {
-      this.mesh = mesh
-      this.vertices = mesh.vertices
-      this.color = { r: colorArrayByVertex[0], g: colorArrayByVertex[1], b: colorArrayByVertex[2] }
-      this.setWireframe = mesh.setWireframe
-      this.matrix = Matrix()
-    }
-    transform = (otherMatrix) => this.matrix = this.matrix.multiply(otherMatrix)
-  } 
-
-  return new ThreeDObject(mesh, colorArrayByVertex)
+  let matrix = Matrix()
+  return {
+    mesh,
+    get vertices() { return mesh.vertices },
+    get matrix() { return matrix },
+    get color() { return { r: colorArrayByVertex[0], g: colorArrayByVertex[1], b: colorArrayByVertex[2] } },
+    setWireframe: mesh.setWireframe,
+    transform: (otherMatrix) => matrix = matrix.multiply(otherMatrix)
+  }
 }
+
+
 
 const Our3DGroup = () => {
   const group = []
   return {
-    group,
+    get group() { return group },
     add: object => group.push(object),
     transform: matrix => group.forEach((object) => object.transform(matrix))
   }
