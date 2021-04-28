03f1804b54a4780215e9ffb05655ede7277baab5
igreen1 <iangreen2.00@gmail.com>
2021-04-28 11:34:03 -0700

    Cleaned up OurScene.js for readability

--- src/scenes/OurScene.js
+++ src/scenes/OurScene.js
@@ -18,10 +18,11 @@ import {
 // Alternatively can import as
 // import * as LIBRARY from './OurLibrary/OurLibrary
 
+// Placed into factories to make ExampleUniverse easier to read
+//  moderate loss of load efficiency is worth readability!!
+const StarFactory = () => {
 
-const ExampleUniverse = () => {
-  let universe = BigBang()
-  let star = Our3DObject(
+  const star = Our3DObject(
     OurMesh(
       Geometries.Extrude(
         [
@@ -55,7 +56,7 @@ const ExampleUniverse = () => {
   star.transformVertices(MatrixLibrary.rotationMatrix(0.5, 0.5, 0.5))
   star.transformVertices(MatrixLibrary.translationMatrix(0.8, 0.4, 1.9))
 
-  let star2 = Our3DObject(
+  const star2 = Our3DObject(
     OurMesh(
       Geometries.Extrude(
         [
@@ -89,23 +90,22 @@ const ExampleUniverse = () => {
   star2.transform(MatrixLibrary.rotationMatrix(0.5, 0.5, 0.5))
   star2.transform(MatrixLibrary.translationMatrix(0.79, 0.4, 1.9))
 
-  let stars = Our3DGroup()
+  const stars = Our3DGroup()
   stars.add(star)
   stars.add(star2);
 
-  universe.addAnimation(
-    Animations.RotateAboutPoint(stars, [0.79, 0.4, 1.9], [0.01, 0.01, 0.01])
-  )
+  return stars;
 
-  universe.addToUniverse(stars)
+}
 
-  let sphere = Our3DObject(OurMesh(Geometries.Sphere(0.3, 5), false), [0, 0, 0])
+const IceCreamFactory = () => {
+  const sphere = Our3DObject(OurMesh(Geometries.Sphere(0.3, 5), false), [0, 0, 0])
   sphere.setRandomColors(10)
   sphere.transform(MatrixLibrary.scaleMatrix(2, 2, 2))
   sphere.transform(MatrixLibrary.rotationMatrix(0, 0, 0.5))
   sphere.transform(MatrixLibrary.translationMatrix(3, -.2, .5))
 
-  let cone = Our3DObject(OurMesh(Geometries.Cone(0.5, 1, 8, 8), false), [0.7, 0, 0.8])
+  const cone = Our3DObject(OurMesh(Geometries.Cone(0.5, 1, 8, 8), false), [0.7, 0, 0.8])
   cone.setRandomColors(5, false)
   cone.transform(MatrixLibrary.rotationMatrix(0, -0.3, 3.14))
   cone.transform(MatrixLibrary.translationMatrix(3, -1, 0.5))
@@ -113,31 +113,42 @@ const ExampleUniverse = () => {
   const IceCream = Our3DGroup()
   IceCream.add(cone)
   IceCream.add(sphere)
-  universe.addToUniverse(IceCream)
+  
+  return IceCream
+}
 
+const PyramidFactory = (position) => {
+  let pyramid = Our3DObject(OurMesh(Geometries.Cone(2.5, 3, 4, 4), false), [1, 1, .1])
+  pyramid.transform(MatrixLibrary.translationMatrix(...position))
+  return pyramid
+}
+
+
+const ExampleUniverse = () => {
+  let universe = BigBang()
+  
+  // Yummy :)
+  const IceCream = IceCreamFactory()
+  universe.addToUniverse(IceCream) // Demonstrating animations can be added after addToUniverse call
   universe.addAnimation(
     Animations.RotateAboutPoint(IceCream, [3, -1, 0.5], [0, 0, 0.1])
   )
 
-  let pyramid = Our3DObject(OurMesh(Geometries.Cone(2.5, 3, 4, 4), false), [1, 1, .1])
-  pyramid.transform(MatrixLibrary.rotationMatrix(0, 0, 0))
-  pyramid.transform(MatrixLibrary.translationMatrix(-.2, -1, -3))
+  // Pyramids
+  const pyramid = PyramidFactory([-.2, -1, -3])
+  const pyramid2 = PyramidFactory([-4, -1, -4])
+  const pyramid3 = PyramidFactory([4, -1, -4])
   universe.addToUniverse(pyramid)
-
-  // universe.addAnimation(
-  //   Animations.RotateAboutPoint(pyramid, [-0.2, -1, -3], [0, 0, 0.1])
-  // )
-
-  let pyramid2 = Our3DObject(OurMesh(Geometries.Cone(2.5, 3, 4, 4), false), [1, 1, .1])
-  pyramid2.transform(MatrixLibrary.rotationMatrix(0, 0, 0))
-  pyramid2.transform(MatrixLibrary.translationMatrix(-4, -1, -4))
   universe.addToUniverse(pyramid2)
-
-  let pyramid3 = Our3DObject(OurMesh(Geometries.Cone(2.5, 3, 4, 4), false), [1, 1, .1])
-  pyramid3.transform(MatrixLibrary.rotationMatrix(0, 0, 0))
-  pyramid3.transform(MatrixLibrary.translationMatrix(4, -1, -4))
   universe.addToUniverse(pyramid3)
 
+
+  // Amazing background :)
+  const stars = StarFactory();
+  universe.addAnimation(
+    Animations.RotateAboutPoint(stars, [0.79, 0.4, 1.9], [0.01, 0.01, 0.01])
+  )
+  universe.addToUniverse(stars)
 
   let ground = Our3DObject(OurMesh(Geometries.RegularPolygon(4), false), [.5, .4, .2])
   ground.transform(MatrixLibrary.scaleMatrix(15, 15, 1))
@@ -151,13 +162,16 @@ const ExampleUniverse = () => {
   sky.transform(MatrixLibrary.translationMatrix(0, 0, -3))
   universe.addToUniverse(sky)
 
+  // From our cast
   universe.addToUniverse(sphinx)
 
+  // We have to see something!
   const camera = OurCamera([0, 1, -5], [0, 0, 0], [0.5, -0.5, 1, -1, 1, 10])
   universe.addToUniverse(camera)
 
   const light = OurLight([-2, 0, 10], [11, 9.2, 9])
   universe.addToUniverse(light)
+
   return universe
 }
 
