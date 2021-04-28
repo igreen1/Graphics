aebd1b557e18ddb10652f085497d1706ef43016a
veronicabp <54556705+veronicabp@users.noreply.github.com>
2021-04-28 15:19:20 -0700

    change sphinx to sphinx factory

--- src/objects/sphinx.js
+++ src/objects/sphinx.js
@@ -1,36 +1,39 @@
 import { Our3DObject, OurMesh, Our3DGroup } from '../VIBAH/Our3DObject'
-import { Cylinder, Sphere } from '../VIBAH/OurGeometryLibrary'
+import { Cylinder, Sphere, } from '../VIBAH/OurGeometryLibrary'
 import { MatrixLibrary } from '../VIBAH/OurMatrix'
+  
+  const SphinxFactory = () => {
+  
+    // Define our body parts
+    const leg1 = Our3DObject(OurMesh(Cylinder(.2, 3, 8, 8), false), [1, 1, .1])
+    const leg2 = Our3DObject(OurMesh(Cylinder(.2, 3, 8, 8), false), [1, 1, .1])
+    const legs = Our3DGroup([leg1, leg2])
+    
+    const body = Our3DObject(OurMesh(Cylinder(.5, 2, 8, 8), false), [1, 1, .1])
 
-let leg1 = Our3DObject(OurMesh(Cylinder(0.2, 3, 8, 8), false), [1, 1, 0.1])
-leg1.transform(MatrixLibrary.rotationMatrix(0, Math.PI / 4, 0))
-leg1.transform(MatrixLibrary.translationMatrix(-4, -2, -1))
-let leg2 = Our3DObject(OurMesh(Cylinder(0.2, 3, 8, 8), false), [1, 1, 0.1])
-leg2.transform(MatrixLibrary.rotationMatrix(0, Math.PI / 4, 0))
-leg2.transform(MatrixLibrary.translationMatrix(-5, -2, -1))
-let body = Our3DObject(OurMesh(Cylinder(0.5, 2, 8, 8), false), [1, 1, 0.1])
-body.transform(MatrixLibrary.rotationMatrix(0, Math.PI / 4, 0))
-body.transform(MatrixLibrary.translationMatrix(-4.5, -1.5, -1))
-let head = Our3DObject(OurMesh(Sphere(0.5, 5), false), [1, 1, 0.1])
-head.transform(MatrixLibrary.rotationMatrix(0, Math.PI / 4, 0))
-head.transform(MatrixLibrary.translationMatrix(-3.2, -1, 0.3))
-let mouth = Our3DObject(OurMesh(Sphere(0.1, 5), false), [0.3, 0.3, 0])
-mouth.transform(MatrixLibrary.rotationMatrix(0, Math.PI / 4, 0))
-mouth.transform(MatrixLibrary.translationMatrix(-2.9, -1.1, 0.7))
-// let ltoe1 = Our3DObject(OurMesh(Cylinder(.05, 1, 8, 8), false), [1, 1, .1])
-// ltoe1.transform(MatrixLibrary.rotationMatrix(0,Math.PI/4,0))
-// ltoe1.transform(MatrixLibrary.translationMatrix(-4,-2,-1))
+    const face = Our3DObject(OurMesh(Sphere(0.5, 5), false), [1, 1, .1])
+    const mouth = Our3DObject(OurMesh(Sphere(0.1, 5), false), [.3,.3,0])
+    const head = Our3DGroup([face, mouth])
 
-// let leftToes = Our3DGroup([ltoe1])
+  
+    // Orientate everything
+    legs.transform(MatrixLibrary.rotationMatrix(0,Math.PI/4,0))
+    head.transform(MatrixLibrary.rotationMatrix(0, Math.PI/4, 0))
+    body.transform(MatrixLibrary.rotationMatrix(0,Math.PI/4,0))
 
-let sphinx = Our3DGroup()
-sphinx.add(leg1)
-sphinx.add(leg2)
-sphinx.add(body)
-sphinx.add(head)
-sphinx.add(mouth)
+    // Put everything in position
+    leg1.transform(MatrixLibrary.translationMatrix(-4,-2,-1))
+    leg2.transform(MatrixLibrary.translationMatrix(-5,-2,-1))
+    body.transform(MatrixLibrary.translationMatrix(-4.5,-1.5,-1))
+    head.transform(MatrixLibrary.translationMatrix(-3.2, -1, .3))
+    mouth.transform(MatrixLibrary.translationMatrix(-2.9, -1.1, .7))
 
-// sphinx.transform(MatrixLibrary.scaleMatrix(.5,.5,.5))
+    const Sphinx = Our3DGroup([legs,body,head])
+
+    // sphinx.transform(MatrixLibrary.scaleMatrix(.5,.5,.5))
 // sphinx.transform(MatrixLibrary.translationMatrix(-2,-1,0))
+  
+    return Sphinx;
+  }
 
-export { sphinx }
+export{SphinxFactory}
