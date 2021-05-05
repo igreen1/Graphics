f026da034aa929ba7a94f1c9eeb5eb3a38e58b94
hallegv <hallevogelpohl@gmail.com>
2021-05-04 19:18:14 -0700

    Threw some objects in OurSandbox.js

--- src/scenes/OurSandbox.js
+++ src/scenes/OurSandbox.js
@@ -1,36 +1,30 @@
 // Import our library
-import {
-  // Geometries,
-  ReactWebGL,
-  BigBang,
-  // OurMesh,
-  // Our3DGroup,
-  // Our3DObject,
-  OurAmbientLight,
-  OurCamera
-  // MatrixLibrary,
-  // Animations
-} from '../VIBAH/VIBAH'
+import { ReactWebGL, BigBang, OurMesh, Our3DObject, OurAmbientLight, OurCamera, Animations } from '../VIBAH/VIBAH'
 
-// import { Cylinder, Sphere, Cone, Lathe } from '../VIBAH/OurGeometryLibrary'
-// import { CamelFactory } from '../objects/camel'
-// import { IceCreamFactory } from '../objects/Detroit'
+import { Cylinder } from '../VIBAH/OurGeometryLibrary'
+import { IceCreamFactory } from '../objects/Detroit'
 import { Vineyard } from '../objects/vineyard'
+import { CamelFactory } from '../objects/camel'
 
 const Sandbox = () => {
   const universe = BigBang()
 
-  // let leftLeg = Our3DObject(OurMesh(Cylinder(0.2, 1.5, 8, 32), false), [1, 1, 0.1])
-  // universe.addToUniverse(leftLeg)
+  // Leg lol
+  let leftLeg = Our3DObject(OurMesh(Cylinder(0.2, 1.5, 8, 32), false), [1, 1, 0.1])
+  universe.addToUniverse(leftLeg)
 
-  // // Yummy :)
-  // const IceCream = IceCreamFactory()
-  // universe.addToUniverse(IceCream) // Demonstrating animations can be added after addToUniverse call
-  // universe.addAnimation(Animations.RotateAboutPoint(IceCream, [3, -1, 0.5], [0, 0, 0.1]))
-  //
+  // Yummy :)
+  const IceCream = IceCreamFactory()
+  universe.addToUniverse(IceCream) // Demonstrating animations can be added after addToUniverse call
+  universe.addAnimation(Animations.RotateAboutPoint(IceCream, [3, -1, 0.5], [0, 0, 0.1]))
 
-  const grapes = Vineyard().Bunch
+  // grapes
+  const grapes = Vineyard().Bunch.translate(0.5, -1, -0.2)
   universe.addToUniverse(grapes)
+
+  // camel
+  const camel = CamelFactory().scale(0.5, 0.5, 0.5).translate(-2.5, 0, 0)
+  universe.addToUniverse(camel)
 
   // We have to see something!
   const camera = OurCamera([0, 1, -5], [0, 0, 0], [0.5, -0.5, 1, -1, 1, 10])
